175b1ec7f84dad57d11620c48d9314f2555fcd56
Leschenko1001 <158287421+Leschenko1001@users.noreply.github.com>
2024-03-13 14:51:49 +0200

    додали рух гравця

--- game.js
+++ game.js
@@ -49,12 +49,6 @@ function create() {
       //Додаємо платформи
  platforms = this.physics.add.staticGroup();
 
- //platforms.create(550, 700, 'ground').setScale(2).refreshBody();
-
- //platforms.create(600, 400, 'ground');
- //platforms.create(50, 250, 'ground');
- //platforms.create(700, 220, 'ground');
-
 
  //Створення землі на всю ширину
  for (var x = 0; x < worldWidth; x = x + 384) {
@@ -69,9 +63,33 @@ function create() {
 //додаємо гравця
  player = this.physics.add.sprite(100, 450, 'dude');
  player.setBounce(0.2);
- player.setCollideWorldBounds(true);
+ player.setCollideWorldBounds(false);
+ player.setDepth(5)
 
- 
+ this.cameras.main.setBounds(0, 0, worldWidth, 1080);
+ this.physics.world.setBounds(0, 0, worldWidth, 1080);
+
+ this.anims.create({
+    key: 'left',
+    frames: this.anims.generateFrameNumbers('dude', { start: 0, end: 3 }),
+    frameRate: 10,
+    repeat: -1
+});
+
+this.anims.create({
+    key: 'turn',
+    frames: [ { key: 'dude', frame: 4 } ],
+    frameRate: 20
+});
+
+this.anims.create({
+    key: 'right',
+    frames: this.anims.generateFrameNumbers('dude', { start: 5, end: 8 }),
+    frameRate: 10,
+    repeat: -1
+});
+ //додали курсор 
+ cursors = this.input.keyboard.createCursorKeys();
  
  
     }   
@@ -82,16 +100,50 @@ function create() {
         repeat: 111,
         setXY: { x: 12, y: 0, stepX: 90 }
     });
+    stars.children.iterate(function (child) {
 
+        child.setBounceY(Phaser.Math.FloatBetween(0.4, 0.8));
+    
+    });
     
     
 
     this.physics.add.collider(player, platforms);
     this.physics.add.collider(stars, platforms);
+    this.physics.add.overlap(player, stars, collectStar, null, this);
     
 }
+//scoreText = this.add.text(16, 16, 'score: 0', { fontSize: '32px', fill: '#000' });
 
-function update() 
+function update() {
+
+    //Додали керування персонажем
+    if (cursors.left.isDown) {
+        player.setVelocityX(-160);
+        player.anims.play('left', true);
+    }
+     else if (cursors.right.isDown) {
+        player.setVelocityX(160);
+        player.anims.play('right', true);
+    } 
+    else {
+        player.setVelocityX(0);
+        player.anims.play('turn');
+    }
+
+    if (cursors.up.isDown && player.body.touching.down) {
+        player.setVelocityY(-480);
+    }
+     
+    {
+       // if (gameOver) {
+          //  return;
+       // }
+}
+}
+//Додали збираня зірок
+function collectStar(player, star) 
 {
-   
+    star.disableBody(true, true);
+    
 }
